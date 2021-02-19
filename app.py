b16b6749954287fa25e97af31f682108209d8872
Ram A <ram.callm@gmail.com>
2021-02-19 09:56:33 +0530

    Completed API Integration and display movie details

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def index():
     return render_template('index.html')
 
 
+
 @app.route('/result', methods=['POST', 'GET'])
 def result():
     if request.method == 'POST':
@@ -21,9 +22,14 @@ def result():
                 'https://api.themoviedb.org/3/search/movie?api_key=a0baf7fe65087ee95486de5163e616ed&language=en-US'
                 '&query="' + movies[0] + '"&page=1')
             print(response.status_code)
-            dat = response.json()['page']
-            print(dat)
-            return render_template('res.html', result=movies, data=dat)
+            dat = response.json()
+            poster = "https://image.tmdb.org/t/p/w185/"+dat["results"][0]["poster_path"]
+            rdate = dat["results"][0]["release_date"]
+            overview = dat["results"][0]["overview"]
+            rating = dat["results"][0]["vote_average"]
+            votes = dat["results"][0]["vote_count"]
+            print(poster)
+            return render_template('res.html', result=movies, poster=poster, rdate=rdate, overview=overview, rating=rating, votes=votes)
         else:
 
             return render_template('notfound.html', result=movies)
